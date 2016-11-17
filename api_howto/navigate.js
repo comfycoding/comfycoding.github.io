b200af1179b447d66c988c9e1b7df08baa558f53
Darlene Zouras <darZouras@Darlenes-MacBook-Pro.local>
2016-11-17 17:15:43 -0600

    API How-to has completed and published

---
+++ api_howto/navigate.js
@@ -0,0 +1,6 @@
+document.getElementById("nav").innerHTML =
+'<a href="index.html">How-To Home</a> ' +
+'- <a href="gettingStarted.html">Getting Started</a> ' +
+'- <a href="buildJS.html">Building Your Application</a> ' +
+'- <a href="generateListings.html">Generate Shop Listings</a> ' +
+'- <a href="finalProduct.html">Final Product</a>';
--- api_howto/test.js
+++ api_howto/test.js
@@ -1,9 +1,25 @@
+var baseURL = "https://openapi.etsy.com/v2/shops/";
 var apiKey = "yytys7tcm73htzzb4f7npm70";
+var shopName = "emandsprout";
 
-function getData(data) {
-      if (data.ok) {
-            break;
-        } else {
-            alert(data.error);
+function displayListings() {
+  $.ajax({
+    url: baseURL + shopName + "/listings/active.js?api_key=" + apiKey +
+    "&includes=MainImage&fields=url,price,title,description&limit=10",
+    dataType: "jsonp",
+
+    success: function (resp) {
+      for (i = 0; i < resp.results.length; i++) {
+        $('#shop').append('<div style="display: inline-block"><a href="' + resp.results[i].url +
+        '" target="_blank">' + '<img alt="' + resp.results[i].description + '" src="' +
+        resp.results[i].MainImage.url_170x135 +
+        '" title="' + resp.results[i].title + '"></img></a><center>$' +
+        resp.results[i].price + '</div>');
       }
+    }
+  });
 }
+
+$(document).ready(function () {
+  displayListings();
+});
